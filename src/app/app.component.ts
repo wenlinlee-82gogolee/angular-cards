c16396760c46f75e65202ad890664cd847ba39dc
Wen-Lin Lee <82gogolee@gmail.com>
2022-09-13 21:33:25 +0800

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,33 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {
+      title: 'Neat Tree',
+      username: 'nature',
+      imageUrl: 'assets/tree.jpeg',
+      content: 'I saw this awesome tree duriing my hike today',
+    },
+    {
+      title: 'Snowy Mountain',
+      username: 'mountainlover',
+      imageUrl: 'assets/mountain.jpeg',
+      content: 'Here is a picture of a snowy mountain',
+    },
+    {
+      title: 'Mountain Biking',
+      username: 'bikingrock',
+      imageUrl: 'assets/biking.jpeg',
+      content: 'I did some biking today',
+    },
+    {
+      title: 'Mountain Biking',
+      username: 'bikingrock',
+      imageUrl: 'assets/biking.jpeg',
+      content: 'I did some biking today',
+    },
+  ];
 }
